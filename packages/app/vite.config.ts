7c6e827eb8c68131c8ea5ca60a4101c23fcae225
Samuel Gbafa <skgbafa@gmail.com>
2025-08-17 01:16:57 -0400

    configured app to build

---
+++ packages/app/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+      "@/components": path.resolve(__dirname, "./components"),
+      "@/hooks": path.resolve(__dirname, "./hooks"),
+      "@/types": path.resolve(__dirname, "./types"),
+      "@/data": path.resolve(__dirname, "./data"),
+      "@/styles": path.resolve(__dirname, "./styles"),
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+})
